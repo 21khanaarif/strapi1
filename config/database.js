e26997b114f8db20501bdcde768a27930c657e5f
21khanaarif <21khanaarif@gmail.com>
2024-11-24 22:53:30 +0530

    changed

--- config/database.js
+++ config/database.js
@@ -1,41 +1,41 @@
-//personal
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'aarif_db'), // replace with your database name
-      user: env('DATABASE_USERNAME', 'postgres'), // replace with your PostgreSQL username
-      password: env('DATABASE_PASSWORD', 'aarifkhan'), // replace with your PostgreSQL password
-      ssl: env.bool('DATABASE_SSL', false),
-    },
-    debug: false,
-  },
-});
-
-
-
-
-
-
-
-
-// //render 
-
+// //personal
 // module.exports = ({ env }) => ({
 //   connection: {
 //     client: 'postgres',
 //     connection: {
-//       host: env('DATABASE_HOST', 'dpg-csu7jutumphs738nd880-a.frankfurt-postgres.render.com'),
+//       host: env('DATABASE_HOST', 'localhost'),
 //       port: env.int('DATABASE_PORT', 5432),
-//       database: env('DATABASE_NAME', 'aarifstrapidb'),
-//       user: env('DATABASE_USERNAME', 'aarifstrapidb_user'),
-//       password: env('DATABASE_PASSWORD', 'fO3k1eQofNgtpu6bP3gtpc9bvrFV83hX'),
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
+//       database: env('DATABASE_NAME', 'aarif_db'), // replace with your database name
+//       user: env('DATABASE_USERNAME', 'postgres'), // replace with your PostgreSQL username
+//       password: env('DATABASE_PASSWORD', 'aarifkhan'), // replace with your PostgreSQL password
+//       ssl: env.bool('DATABASE_SSL', false),
 //     },
 //     debug: false,
 //   },
 // });
+
+
+
+
+
+
+
+
+//render 
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'dpg-csu7jutumphs738nd880-a.frankfurt-postgres.render.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'aarifstrapidb'),
+      user: env('DATABASE_USERNAME', 'aarifstrapidb_user'),
+      password: env('DATABASE_PASSWORD', 'fO3k1eQofNgtpu6bP3gtpc9bvrFV83hX'),
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    debug: false,
+  },
+});
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,42 @@
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
+
+
+
+
+
+
+
+
+
 module.exports = [
+  'strapi::errors', // Add this line
   'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'], // Ensure 'res.cloudinary.com' is included
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
